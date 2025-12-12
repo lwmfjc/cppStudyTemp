e751e3b8d8586f5608b7a92f4c7573855560b24d
lwmfjc <lwmfjc@gmail.com>
2025-12-13 01:09:43 +0800

    auto

--- ChernoCpp/HelloWorld/HelloWorld/Log.cpp
+++ ChernoCpp/HelloWorld/HelloWorld/Log.cpp
@@ -1,8 +1,12 @@
 /*试着编译单个cpp文件 ctrl+f7
 会生成Log.obj文件*/
+
+//如果没有这句，会提示cout不是std的成员，因为
+//没有包含这样函数的声明
 #include <iostream>
 
+
 //定义，定义这个函数到底是什么
-void Log(const char* message) {
+void Logr(const char* message) {
 	std::cout << message << std::endl;
 }
--- ChernoCpp/HelloWorld/HelloWorld/Main.cpp
+++ ChernoCpp/HelloWorld/HelloWorld/Main.cpp
@@ -1,7 +1,7 @@
 /*试着编译单个cpp文件 ctrl+f7
 会生成Main.obj文件*/
 
-/*/①编译器处理）include是预处理指令，会在编
+/*/①（编译器处理）include是预处理指令，会在编
 译前找到iostream这个文件并插入该文件内容*/
 #include <iostream>
 
@@ -10,10 +10,10 @@
 .cpp文件会被编译成 .obj文件，链接器把这些合并成 .exe文件*/
 
 /*③声明，确实有Log函数存在*/
-/*④链接器，编译整个项目时，链接器链接时才会去查找这个
-函数是否真是存在,如果不存在，则会提示"unresolved external
-symbol"：未解析的外部符号，链接器无法找到定义
----链接器的工作是将函数体链接到其标识符(名称)
+/*④链接器，编译整个项目时，(main中调用了该Log函数才会去链接)链接器链接时
+才会去查找这个函数是否真是存在,如果不存在，则会提示"unresolved external
+symbol"：未解析的外部符号，链接器无法找到定义---链接器的工作是将函数体链
+接到其标识符(名称)
 */
 //参数名message可以省略
 void Log(const char* message);
@@ -21,11 +21,15 @@ void Log(const char* message);
 
 int main() {
 	// << 重载运算符, 运算符可以理解成函数
-	//类似 std::cout.print("hello world").println()
+	//类似下面这句：
+	// std::cout.print("hello world!").print(std:endl);//!!编译无法通过
+	
 	//std::cout << "hello world!" << std::endl;
-	// 
+	
+	//演示Log函数
 	Log("helloworld!");
-	//等待接收字符
+	
+	//等待按下回车键来移动到程序的下一行
 	std::cin.get();
 	//可以不写return，默认返回0，表示正确返回程序
 }
