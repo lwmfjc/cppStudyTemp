67e2e4ab46b722c6b87546b4e6e8cbf5a9d7a450
lwmfjc <lwmfjc@gmail.com>
2025-12-14 00:01:56 +0800

    auto

--- ChernoCpp/HelloWorld/HelloWorld/Math.cpp
+++ ChernoCpp/HelloWorld/HelloWorld/Math.cpp
@@ -1,4 +1,9 @@
+const char* Log(const char* message) {
+	return message;
+}
+
 int Multiply(int a, int b) {
-	int result = a * b;
-	return result;
+
+	Log("Multiply");
+	return  a * b;
 }
---
+++ ChernoCpp/HelloWorld01/HelloWorld01/Math.cpp
@@ -0,0 +1,10 @@
+ 
+
+#pragma comment(linker, "/ENTRY:haha") 
+
+// 这个main函数会被自动调用
+int haha()
+{
+     
+    return 0;
+}
