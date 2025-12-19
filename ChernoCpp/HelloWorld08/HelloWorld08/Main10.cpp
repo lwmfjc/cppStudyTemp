c6bf64fbd766356ed99642ec262206725a933d3f
lwmfjc <lwmfjc@gmail.com>
2025-12-19 23:01:46 +0800

    auto

--- ChernoCpp/HelloWorld08/HelloWorld08/Main10.cpp
+++ ChernoCpp/HelloWorld08/HelloWorld08/Main10.cpp
@@ -2,16 +2,18 @@
 #include "Log.h" 
 
 
-int main() {
-	//InitLog();
-	int a = 257;
-	a++;
-	const char* string = "Hello";
-	for (int i = 0; i < 5; i++) {
-		const char c = string[i];
-		std::cout << c << std::endl;
+int main() 
+{
+	int x = 6;
+	
+	//比较这里比较int的4字节的
+	// 每个位是否相等
+	bool comparisionResult =
+		x == 5;
+	if (comparisionResult)
+	{
+		Log("hello world!");
 	}
-	Log("hello world!");
-	//std::cin.get();
+
 	std::cin.get();
 }
