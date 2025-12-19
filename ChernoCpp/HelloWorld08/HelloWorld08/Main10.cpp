9883b242692b832c01432adbb4978f148a07c403
lwmfjc <lwmfjc@gmail.com>
2025-12-20 00:06:06 +0800

    auto

--- ChernoCpp/HelloWorld08/HelloWorld08/Main10.cpp
+++ ChernoCpp/HelloWorld08/HelloWorld08/Main10.cpp
@@ -8,12 +8,17 @@ int main()
 	
 	//比较这里比较int的4字节的
 	// 每个位是否相等
-	bool comparisionResult =
-		x == 5;
+	bool comparisionResult  = x == 5;
 	if (comparisionResult)
 	{
 		Log("hello world!");
 	}
-
+	const char* ptr = "hello";
+	if (ptr)
+		Log(ptr); 
+	else if (ptr=="hello")
+	{
+		Log("ptr is hello!");
+	}
 	std::cin.get();
 }
