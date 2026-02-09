c1329e46f7180a96138673dc9c4d163373173ec3
lwmfjc <lwmfjc@gmail.com>
2026-02-10 00:37:48 +0800

    auto-update: 2026-02-10_00:37:48

--- ChernoCpp/HelloWorld52/HelloWorld52/Main54_1.cpp
+++ ChernoCpp/HelloWorld52/HelloWorld52/Main54_1.cpp
@@ -13,19 +13,23 @@ struct Vector3
 
 int main()
 {
-	//在栈上分配内存
-	int value = 5;
-	int array[5];
-	array[0] = 1;
-	array[1] = 2;
-	array[2] = 3;
-	array[3] = 4;
-	array[4] = 5; 
-	Vector3 vector;
+	{
+		//在栈上分配内存
+		int value = 5;
+		int array[5];
+		array[0] = 1;
+		array[1] = 2;
+		array[2] = 3;
+		array[3] = 4;
+		array[4] = 5;
+		Vector3 vector;
 
-	std::cout << &value << std::endl;
-	std::cout << array << std::endl;
-	std::cout << &vector << std::endl;
+		std::cout << &value << std::endl;
+		std::cout << array << std::endl;
+		std::cout << &vector << std::endl;
+		
+	}//作用域结束后，该作用域内栈上分配所有内容都被弹出（内存被释放）
+	//在栈上分配内存和释放内存都只是移动栈指针，几乎没有性能损耗
 
 	//new关键字分配堆内存
 	int* hvalue = new int;
@@ -38,6 +42,11 @@ int main()
 	harray[4] = 5;
 	//()可以省略
 	Vector3* hvector = new Vector3();
+
+	//必须手动释放内存
+	delete hvalue;
+	delete[] harray;
+	delete hvector;
 
 	std::cin.get();
 }
