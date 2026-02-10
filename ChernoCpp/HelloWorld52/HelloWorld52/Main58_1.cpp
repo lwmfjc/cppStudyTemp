0e6a7adacece0ba7cd7fe8f08559a7e6653b4088
lwmfjc <lwmfjc@gmail.com>
2026-02-11 07:57:03 +0800

    auto-update: 2026-02-11_07:57:03

--- ChernoCpp/HelloWorld52/HelloWorld52/Main58_1.cpp
+++ ChernoCpp/HelloWorld52/HelloWorld52/Main58_1.cpp
@@ -1,7 +1,34 @@
 #include <iostream>  
 
-int main()
-{ 
+void HelloWorld()
+{
+	std::cout << "HelloWorld!" << std::endl;
+}
 
+
+int test(float a)
+{
+	std::cout << a << "!" << std::endl;
+	return 1;
+}
+
+int main()
+{
+	//function的类型->void (*function)()
+	auto function1 = &HelloWorld;
+	//&可以省略，因为有隐式转换
+	auto function = HelloWorld;
+	//相当于
+	void(*function2)() = HelloWorld;
+	int(*function3)(float) = test;
+
+	function();
+	(*function)();
+	function1();
+	(*function1)();
+	function2();
+	(*function2)();
+
+	function3(3.0);
 	std::cin.get();
 }
