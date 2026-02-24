e5210c386187d9b73572dc19a1dec94b0b750194
lwmfjc <lwmfjc@gmail.com>
2026-02-24 23:24:07 +0800

    auto-update: 2026-02-24_23:24:06

--- ChernoCpp/HelloWorld62_End/HelloWorld62/Main68_02.cpp
+++ ChernoCpp/HelloWorld62_End/HelloWorld62/Main68_02.cpp
@@ -5,14 +5,19 @@ class Base
 {
 public:
 	Base() { std::cout << "Base constructor\n"; }
-	~Base() { std::cout << "Base destructor\n"; }
+	
+	//析构函数加virtual，表示这个类可能被拓展（继承），可能会有一个析构函数
+	//需要被调用
+	virtual ~Base()  { std::cout << "Base destructor\n"; }
 };
 
 class Derived : public Base
 {
 public:
-	Derived() { std::cout << "Derived constructor\n"; }
-	~Derived() { std::cout << "Derived destructor\n"; }
+	Derived() { m_array = new int[5];  std::cout << "Derived constructor\n"; }
+	~Derived() { delete[] m_array; std::cout << "Derived destructor\n"; }
+private:
+	int* m_array;
 };
 
 int main()
---
+++ ChernoCpp/HelloWorld62_End/HelloWorld62/Main69_01.cpp
@@ -0,0 +1,11 @@
+#ifdef LY_EP69
+#include <iostream>
+
+int main()
+{
+
+	//成功的情况只有一种（0），而失败的原因
+	//可以有成千上万种（1, 2, 3...）。
+	return 0;
+}
+#endif
