71c66a233ee175cdc469d43a86e872065b94ce2c
lwmfjc <lwmfjc@gmail.com>
2026-03-11 00:09:28 +0800

    auto-update: 2026-03-11_00:09:27

--- ChernoCpp/HelloWorld81_/HelloWorld81_/Main94_01.cpp
+++ ChernoCpp/HelloWorld81_/HelloWorld81_/Main94_01.cpp
@@ -1,9 +1,44 @@
 #ifdef LY_EP94
 
-#include <iostream>    
+#include <iostream>   
+#include <vector>
+#include "94_01_vector.h"
 
 int main()
 {
+	/*std::vector<int> values = { 1,2,3,4,5 };
+
+	for (int i = 0; i < values.size(); i++)
+	{
+		std::cout << values[i] << std::endl;
+
+	}
+
+	for (int value : values)
+	{
+		std::cout << value << std::endl;
+	}
+
+	for (std::vector<int>::iterator it = values.begin();
+		it != values.end(); it++)
+	{
+		std::cout << *it << std::endl;
+	}
+
+	std::cout << "=============" << std::endl;*/
+
+	Vector<int> values;
+	values.EmplaceBack(1);
+	values.EmplaceBack(2);
+	values.EmplaceBack(3);
+	values.EmplaceBack(4);
+	values.EmplaceBack(5);
+
+	std::cout << "Not using iterators:\n";
+	for (int i = 0; i < values.Size(); i++)
+	{
+		std::cout << values[i] << std::endl;
+	}
 
 	std::cin.get();
 	return 0;
