5b924b8335f0181f0d931a8443397f52b2a04014
lwmfjc <lwmfjc@gmail.com>
2026-03-14 20:34:24 +0800

    auto-update: 2026-03-14_20:34:24

--- learnopengl/HelloOpengl/HelloOpengl/Main03_01.cpp
+++ learnopengl/HelloOpengl/HelloOpengl/Main03_01.cpp
@@ -3,9 +3,23 @@
 #include <GLFW/glfw3.h> 
 
 #include <iostream>
+#include <thread> // 包含 sleep_for
+#include <chrono> // 包含时间单位
 
+void processInput(GLFWwindow* window)
+{
+	/*
+	检查用户是否按下了 Esc 键（如果没有按下，glfwGetKey 返回 GLFW\_RELEASE ）。如果用户按下了 Esc 键，我们使用 glfwSetwindowShouldClose 将 GLFW 的 WindowShouldClose 属性设置为 `true` 来关闭 GLFW
+	*/
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+}
+
+
+//该函数在窗口大小变化时回调，每当窗口大小发生变化时，GLFW 都会调用此函数并填充相应的参数供您处理
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
+	//告诉 OpenGL 渲染窗口的大小
 	glViewport(0, 0, width, height);
 }
 
@@ -65,15 +79,64 @@ int main()
 	//重点：视口不一定要填满整个窗口。
 	//glViewport(0, 0, 800, 600);
 
-	// 注册窗口大小改变的回调
+	// 注册 窗口大小改变的回调
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+	int i = 0;
+
+	//1. 问：该下班（关窗口）了吗？
 	while (!glfwWindowShouldClose(window))
-	{
-		glfwSwapBuffers(window);
+	{//顺序原则：先取样（Poll），再处理（Input），后绘制（Render），末提交（Swap）
+
+		//check and call events
+		//这个函数负责处理所有的互动
+		//它去操作系统那里询问：“刚才这 0.01 秒里，用户动鼠标了吗？按 ESC 键了吗；如果你之前注册过“按下 ESC 就关窗口”的函数，glfwPollEvents 发现你按了 ESC，就会立刻跳过去执行你的那个函数
+		// 1. 查：刚才用户有没有搞什么小动作（按键、移动鼠标）？
 		glfwPollEvents();
+
+
+		//input
+		//2.算：根据输入处理逻辑（Process Input / Update Logic）
+		processInput(window);
+
+		//这里稍微改下逻辑，验证了确实是双缓冲区处理图像
+		if (i % 2 == 0)
+		{
+			i++;
+			// 3. 画：清屏并执行渲染指令（Rendering）
+			// 这里通常会写 glClear() 和 glDraw... (开始在“后台”画画)
+			// rendering commands here
+			//设置rgba值，20% 的红，30% 的绿，30% 的蓝，100%不透明
+			glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		}
+		else
+		{
+			i--;
+			//设置rgba值，30% 的红，20% 的绿，10% 的蓝，100%不透明
+			glClearColor(0.3f, 0.2f, 0.1f, 1.0f);
+
+		}
+
+		//立刻把 GL_COLOR_BUFFER_BIT（颜色缓冲区）里所有的像素，全部涂成我刚才在 glClearColor 里指定的颜色
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		//swap the buffers
+		/*
+	*单缓冲（坏处）：如果你直接在观众面前画，观众会看到你先
+	  画背景，再画人物，最后上色。这在屏幕上会导致严重的闪烁，因为观众看到了“半成品”。
+	* 双缓冲（glfwSwapBuffers）：
+		* 后缓冲区 (Back Buffer)：这是隐藏在后台的画布。所
+		  有的 glClear、glDraw... 都在这里偷偷进行。观众看不见。
+		* 前缓冲区 (Front Buffer)：这是目前正在屏幕上显示的成品图。
+		* glfwSwapBuffers 的动作：当你在后台画好了完整的一帧，这行代码会瞬间把“前台”和“后台”交换。观众看的是上一秒画好的，而你已经在准备下一秒的了。
+*/
+// 4. 喊：画好了！把画完的后台画布翻到前台给用户看！
+		glfwSwapBuffers(window);
+
 	}
 
+	//渲染循环结束后，我们需要彻底清理 / 删除所有已分配的 GLFW 资源
+	glfwTerminate();
 	return 0;
 }
 
