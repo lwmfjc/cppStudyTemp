0b66a709ddd985ba6429bc20a09f20c80b2fb55c
lwmfjc <lwmfjc@gmail.com>
2026-03-18 18:31:48 +0800

    auto-update: 2026-03-18_18:31:47

--- learnopengl/HelloOpengl/HelloOpengl/shader/shader_08.fs
+++ learnopengl/HelloOpengl/HelloOpengl/shader/shader_08.fs
@@ -17,6 +17,7 @@ void main()
     //这里给他强行加了奇怪的效果
     //这个不是向量乘积，a * b = (ar* br,ag*bg,ab*bb,aa*ba)
     //FragColor = texture(ourTexture, TexCoord) * vec4(ourColor, 1.0);
+    //颜色是片段着色器拿着这个纹理坐标去纹理采样器查的。”
     FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
     //FragColor = mix(texture(texture1, TexCoord), texture(texture2, vec2(-TexCoord.s,TexCoord.t)), 0.2);
 
