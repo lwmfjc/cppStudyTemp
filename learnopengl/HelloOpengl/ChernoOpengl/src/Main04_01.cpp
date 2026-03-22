9e16da0c94771bb97f41839e6c8eb50465ebc746
lwmfjc <lwmfjc@gmail.com>
2026-03-22 18:44:45 +0800

    auto-update: 2026-03-22_18:44:44

--- learnopengl/HelloOpengl/ChernoOpengl/src/Main04_01.cpp
+++ learnopengl/HelloOpengl/ChernoOpengl/src/Main04_01.cpp
@@ -5,12 +5,11 @@
 
 int main(void)
 {
+#pragma region 一些初始化
 	GLFWwindow* window;
-
 	/* Initialize the library */
 	if (!glfwInit())
 		return -1;
-
 	/* Create a windowed mode window and its OpenGL context */
 	window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
 	if (!window)
@@ -18,7 +17,6 @@ int main(void)
 		glfwTerminate();
 		return -1;
 	}
-
 	/* Make the window's context current */
 	glfwMakeContextCurrent(window);
 	//先有窗口和上下文，加载器才能工作
@@ -26,24 +24,52 @@ int main(void)
 	{
 		std::cout << "Error!" << std::endl;
 	}
+#pragma endregion
 
-	//4.6.0 Compatibility Profile Context 23.19.12.02.240618
-	//std::cout << glGetString(GL_VERSION) << std::endl;
+
+	float positions[6] = {
+		-0.5f, -0.5,
+		0.0f, 0.5f,
+		0.5f, -0.5f
+	};
+
+	//用来存储生成的顶点缓冲区id
+	unsigned int buffer;
+	//定义顶点缓冲区，把生成的缓冲区id(唯一的id)存储到给定地址,
+	//之后想使用这个缓冲区对象的时候，传这个id即可
+	glGenBuffers(1, &buffer);
+	// 告诉 OpenGL：把 ID 为 buffer 的这个缓冲区，
+	// “插”进 GL_ARRAY_BUFFER 这个槽位里。
+	// 接下来所有针对 GL_ARRAY_BUFFER 的操作（如填
+	// 充数据、设置格式），都会作用在这个特定的 buffer 上。
+	glBindBuffer(GL_ARRAY_BUFFER, buffer);
+	//对这个槽位操作：填充数据
+	//你告诉显卡：“我打算怎么用这块数据，多久改一次？”显卡会根据你的回答，决定把这块数据放在显存的哪个位置（是读写最快的地方，还是稍微慢一点但方便修改的地方）
+	//STREAM: 数据几乎每一帧都要修改（例如：每帧都在变的粒子效果）。STATIC: 数据只设置一次，之后几乎不动（例如：静态的地形、房屋模型）。DYNAMIC : 数据会频繁修改，但可能不像每帧那么夸张
+	//DRAW: 数据由 CPU 写入，供 GPU 读取用于画图（这是 99% 的情况）。READ: 数据由 GPU 写入，供 CPU 读取（例如：回传渲染结果）。COPY : 数据由 GPU 写入，供 GPU 以后自己读取。
+	glBufferData(GL_ARRAY_BUFFER, 6 * sizeof(float),
+		positions, GL_STATIC_DRAW);
+	//接下来需要告诉GPU，我给的这几个字节的数据该如何读
 
 	/* Loop until the user closes the window */
-   //在while中添加代码
+	//在while中添加代码
 	while (!glfwWindowShouldClose(window))
 	{
 		/* Render here */
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		//===绘制三角形===
-		//glBegin(GL_TRIANGLES);
-		//glVertex2f(-0.5f, -0.5f);
-		//glVertex2f(0.0f, 0.5f);
-		//glVertex2f(0.5f, -0.5f);
-		//glEnd();
-		//===绘制三角形===
+		//给缓冲区发出指令，1：指定绘制的图形
+		//2：指定偏移量（指定数组中起始位置，可以非零）
+		//3：渲染的顶点的数量，而不是画多少个“形状”
+		//这里没有指定任何东西，但是由于OpenGL是一个状态机，
+		//线性读取：从当前绑定在 GL_ARRAY_BUFFER 上的缓冲区中，从第 0 个顶点开始，顺次读取 3 个顶点进行渲染。
+		//所以，drawArrays之前GL_ARRAY_BUFFER一定要绑定着
+		//某一个缓冲区
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+
+		//有个参数填写要绘制的顶点数量
+		//另一个参数指定渲染的顶点顺序
+		//glDrawElements(...);
 
 		/* Swap front and back buffers */
 		glfwSwapBuffers(window);
