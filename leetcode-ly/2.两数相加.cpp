bdaf034ce6df69fb9153bea41b05cd41b0a0f62a
lwmfjc <lwmfjc@gmail.com>
2026-06-09 14:32:43 +0800

    push

---
+++ leetcode-ly/2.两数相加.cpp
@@ -0,0 +1,13 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=cpp
+ *
+ * [2] 两数相加
+ */
+#include <iostream> 
+// @lc code=end
+using namespace std::numbers;
+int main(){
+    std::cout << "helo";
+    printf("%.10lf %.10lf\n",pi,e);
+    return 0;
+}
