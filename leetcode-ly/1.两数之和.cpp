c8a49db332083fb2c288acd34fc097eb1a4a244d
lwmfjc <lwmfjc@gmail.com>
2026-07-27 21:08:25 +0800

    auto-update: 2026-07-27_21:08:25

--- leetcode-ly/1.两数之和.cpp
+++ leetcode-ly/1.两数之和.cpp
@@ -3,85 +3,30 @@
  *
  * [1] 两数之和
  */
-
-// @lc code=start
 #include <vector>
-#include <algorithm>
-#include <iostream>
-#include <bits/stdc++.h>
-
 using namespace std;
+// @lc code=start
 class Solution
 {
 public:
-  vector<int> twoSum(vector<int> &nums, int target)
-  {
-    auto begin{nums.begin()};
-    auto end{nums.end()};
-    // 至少有两个元�?
-    sort(begin, end);
-    for (int i = 0; i < nums.size() - 1; i++)
+    vector<int> twoSum(vector<int> &nums, int target)
     {
-      int findValue = target - nums[i];
-      // 从i+1开始查�?
-      auto find = lower_bound(begin + i + 1, end, findValue);
-      if (find != end)
-      {
-        vector<int> result{i, (int)(find - begin)};
-        return result;
-      }
     }
-    return nums;
-  }
 };
 // @lc code=end
+#include <iostream>
 
-void test(int a[])
+//常量指针：落脚点是“指针”，它是一个可以指向别处的指针，只是指向的内容是常量。
+//指针常量：落脚点是“常量”，它本质上是一个一旦初始化就生死不改的常量，只是这个常量的类型是指针。
+int main(int argc, const char *argv[])
 {
-  a = a + 1;
-
-  cout << *a << endl;
-}
-void test1(char *c)
-{
-  *(c + 1) = '1';
-}
-int main(int argc, char const *argv[])
-{
-  // vector<int> a{3,2,4};
-  // Solution solution;
-  // auto result = solution.twoSum(a,6);
-  // cout << result[0] << endl;
-  // cout << result[1] << endl;
-  // int k1=65536;
-  // int k2=65536;
-  // auto  size=65535UL*65536;
-  // // vector<int> a(size-1);
-  // // cout << a.size() << endl;
-  // cout << size <<endl;
-  // map<int,int> mp;
-  // cout << mp.count(2) << endl;
-  // cout << mp[2] << endl;
-  // cout << mp.count(2) << endl;
-  // int a{3};
-  // int b{5};
-  // swap(a,b);
-  // cout << a << endl;
-
-  int a[]{1, 2, 3, 4, 5};
-  // a=a+1;
-  //  test(a);
-
-    char * c="Hello"; //字符串常�? 不允许修�?
-  char c1[] = "world";
-  //  c+1='3';
-  /*
-  warning: ISO C++ forbids converting a string constant to 'char*' [-Wwrite-strings]
-  */
-    //  test1(c);     
-    // cout << c << endl;
-
-  test1(c1);
-  cout << c1 << endl;
-  return 0;
+    Solution s;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+    vector<int> result = s.twoSum(nums, target);
+    for (int i : result)
+    {
+        cout << i << " ";
+    }
+    return 0;
 }
